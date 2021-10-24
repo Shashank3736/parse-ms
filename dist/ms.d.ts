06a126d84eab75a33fab92266bbb37336f29886f
Shreyash Raj <shreyashr267@gmail.com>
2021-10-24 19:07:48 +0530

    v2.1.0

--- dist/ms.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function parse(text: string, debug?: Boolean): number | undefined;
-export declare function fmShort(milliseconds: number): string;
-export declare function fmLong(milliseconds: number): string;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import { fmLong, parse } from "./ms";
+import { fmLong, fmShort, parse } from "./ms";
 
 function ms(value: string): number;
-function ms(value: number): string;
-function ms(value: string | number ) {
+function ms(value: number, type: 'short' | 'long'): string;
+function ms(value: number | string, type: 'short' | 'long' = 'long') {
     if(typeof value === 'number') {
-        return fmLong(value);
+        if(type === 'short') return fmShort(value);
+        else return fmLong(value);
     } else {
         const array = value.match(/(-?[0-9]+) *(milliseconds?|msecs?|ms|seconds?|secs?|s|minutes?|mins?|m|hours?|hrs?|h|days?|d|weeks?|w|years?|yrs?|y)/gi) || ['0s'];
-        const result = array.reduce((a, b) => 
+        const result = array.reduce((a: string | number, b: string) => 
             (isNaN(a) ? parse(a): a) +
             ((isNaN(a) ? parse(a): a) < 0 ? -parse(b):parse(b))
             , '0s');
